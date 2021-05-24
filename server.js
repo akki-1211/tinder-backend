d17f23568f7ff85c6226ce096af0945b1ce64cc8
akki-1211 <parekhakshat1211@gmail.com>
2021-05-24 19:33:22 +0530

    add apis

--- server.js
+++ server.js
@@ -19,30 +19,30 @@ mongoose.connect(connection_url,{
 })
 
 //Api EndPoints
-app.get('/',(req,res) => {
+app.get('/*',(req,res) => {
     res.status(200).send('hello Geeks!')
 })
 
-app.post('/tinder/cards',(req,res)=>{
-    const dbCard = req.body
-    Cards.create(dbCard,(err, data) => {
-        if(err){
-            res.status(500).send(err)
-        }else{
-            res.status(201).send(data)
-        }
-    })
-})
+// app.post('/tinder/cards',(req,res)=>{
+//     const dbCard = req.body
+//     Cards.create(dbCard,(err, data) => {
+//         if(err){
+//             res.status(500).send(err)
+//         }else{
+//             res.status(201).send(data)
+//         }
+//     })
+// })
 
-app.get('/tinder/cards',(req,res) => {    
-    Cards.find((err, data) => {
-        if(err){
-            res.status(500).send(err)
-        }else{
-            res.status(200).send(data)
-        }
-    })
-})
+// app.get('/tinder/cards',(req,res) => {    
+//     Cards.find((err, data) => {
+//         if(err){
+//             res.status(500).send(err)
+//         }else{
+//             res.status(200).send(data)
+//         }
+//     })
+// })
 
 //Listening
 app.listen(port,()=>console.log(`Listening on localhost: ${port}`));
